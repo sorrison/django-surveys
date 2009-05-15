7dcc2dcba6a572c0c705c735717c0e5c2d4ea3ed
Sam Morrison <sam@vpac.org>
2009-05-15 00:00:20 +0000

    url bug

--- django_surveys/urls.py
+++ django_surveys/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('django_surveys.views',
 
     url(r'^$', 'surveygroup_list', name='surv_surveygroup_list'),
     url(r'^create/$', 'add_edit_survey', name='surv_survey_add'),
-    url(r'^(?P<surveygroup_id>\d+)/edit/$', 'add_edit_survey', name='surv_surveygroup_edit'),
+    url(r'^(?P<surveygroup_id>\d+)/edit/$', 'add_edit_survey', name='surv_survey_edit'),
     url(r'^questions/(?P<question_id>\d+)/$', 'question_detail', name='surv_question_detail'),
 )
