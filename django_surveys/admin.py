3e3e05bd9a5e4fdae27d95ea4fdc16a7a51e8360
Sam Morrison <sam.morrison@arcs.org.au>
2009-05-29 16:56:25 +1000

    added GPL

--- django_surveys/admin.py
+++ django_surveys/admin.py
@@ -1,3 +1,21 @@
+# Copyright 2008 VPAC
+#
+# This file is part of django-surveys.
+#
+# django-surveys is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django-surveys is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django-surveys  If not, see <http://www.gnu.org/licenses/>.
+
+
 from django.contrib import admin
 from models import SurveyGroup, Survey, Question, Answer, TextAnswer, IntegerAnswer, BooleanAnswer, CharAnswer
 
@@ -8,13 +26,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ('survey_group',)
 
 
-
-
-
 admin.site.register(Question, QuestionAdmin)
-
-
-
 admin.site.register(SurveyGroup)
 admin.site.register(Answer)
 admin.site.register(Survey)
