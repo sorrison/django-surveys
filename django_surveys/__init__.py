99041c9982e3639eb24a8bf1f571146b8225d7cf
Sam Morrison <sam@vpac.org>
2010-03-04 02:45:55 +0000

    Include files to use for making debian packages. Run 'make builddeb'. Added version number (1.0). Set in django_surveys.__init__.py

--- django_surveys/__init__.py
+++ django_surveys/__init__.py
@@ -14,3 +14,17 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with django-surveys  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION = (1, 0, '', 'final', 0)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    return version
+
